d33b89ba376c743048dcf7658e21fa9a1ffe9a7a
alaminet <alaminet.bd@gmail.com>
2025-09-08 10:13:29 +0600

    Backend rearrange

--- backend/route/api/master/index.js
+++ backend/route/api/master/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const route = express.Router();
 const itemInfoApi = require("./itemInfo");
-const itemUOMApi = require("./itemUOM");
-const itemGroupApi = require("./itemGroup");
 const itemDetailsApi = require("./itemDetails");
 
 route.use("/itemInfo", itemInfoApi);
-route.use("/itemUOM", itemUOMApi);
-route.use("/itemGroup", itemGroupApi);
 route.use("/itemDetails", itemDetailsApi);
 
 module.exports = route;
