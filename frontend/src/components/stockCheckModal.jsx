e6a69fca887cb155a8ea206be5247c19e9c3accb
Al Amin ET <alaminet.bd@gmail.com>
2026-01-06 03:41:11 +0600

    UOM-added-on-stock-view

--- frontend/src/components/stockCheckModal.jsx
+++ frontend/src/components/stockCheckModal.jsx
@@ -36,6 +36,11 @@ const StockCheckModal = () => {
       key: "name",
     },
     {
+      title: "UOM",
+      dataIndex: "UOM",
+      key: "UOM",
+    },
+    {
       title: "Stock",
       dataIndex: "stock",
       key: "stock",
@@ -55,10 +60,13 @@ const StockCheckModal = () => {
           },
         }
       );
+      console.log(res?.data?.items);
+
       const tableArr = res?.data?.items?.map((item, index) => ({
         key: index + 1,
         SKU: item.SKU,
         name: item.name,
+        UOM: item.UOM?.code,
         stock: item.stock?.reduce((acc, curr) => acc + curr.onHandQty, 0),
         search: `${item.SKU} ${item.name}`,
       }));
@@ -75,7 +83,8 @@ const StockCheckModal = () => {
         icon={<ProductOutlined />}
         type="primary"
         className="borderBrand"
-        style={{ borderRadius: "0px" }}>
+        style={{ borderRadius: "0px" }}
+      >
         Check Stock
       </Button>
       <Modal
@@ -83,7 +92,8 @@ const StockCheckModal = () => {
         footer={false}
         loading={loading}
         open={open}
-        onCancel={() => setOpen(false)}>
+        onCancel={() => setOpen(false)}
+      >
         <Input
           placeholder="Name / SKU"
           variant="filled"
